3785ab5e35f2bd66535064072a488b4126114714
Moore <DMoore@isolvedhcm.com>
2017-06-03 13:56:01 -0600

    Basic work on project.

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,4 +1,17 @@
-devMtIn.controller('mainCtrl', function($scope){
+devMtIn.controller('mainCtrl', function($scope, mainService){
+    var userProfile = window.localStorage.getItem("userProfile");
+    console.log(userProfile);
+    if (window.localStorage.getItem("userProfile") == false){
+        return $scope.header = 'INITIAL VIEW NO DATA';
+    }
+        $scope.header = window.localStorage.getItem('userProfile');
 
-    $scope.test = "Hello world!";
+    $scope.savePersonalData = function(fullName, tagline, profileUrl, bio){
+            var userProfile = mainService.createProfile(fullName, tagline, profileUrl, bio);
+            $scope.header = userProfile.fullName;
+            window.localStorage.setItem("userProfile", JSON.stringify(userProfile));
+            console.log("Locally Stored Item: " + window.localStorage.getItem("userProfile"));
+            return userProfile;
+    }
+    
 });
--- js/mainService.js
+++ js/mainService.js
@@ -1,4 +1,19 @@
 devMtIn.service('mainService', function($http, $q){
 
+    this.createProfile = function(name, tag, url, bio){
+        console.log(name, tag, url, bio);
+        if (name && tag && url && bio){
+            var userProfile = {
+            fullName: name,
+            tagline: tag,
+            profileUrl: url,
+            bio: bio
+        }
+        console.log(userProfile);
+        return userProfile;
+        }
+        console.log("Falsy values passed");
+        alert("Required Field is empty")
+    }
 
 });
